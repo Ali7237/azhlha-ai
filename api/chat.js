e2cb6df3b0248dcf312c91f64d2eff1e795ccb0e
Ali7237 <r.goatstore@gmail.com>
2026-06-22 23:30:42 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
@@ -101,4 +101,4 @@ ${userName && userName !== 'زائر' ? `اسم المستخدم: ${userName}` :
   } catch (err) {
     return res.status(200).json({ reply: `⚠️ خطأ تقني: ${err.message}` });
   }
-}
+};
